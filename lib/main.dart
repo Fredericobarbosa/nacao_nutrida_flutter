ec7583f4924043644a358fd75452e1e8d5a24c33
Jorge Patrocínio <141787356+jorgesantos001@users.noreply.github.com>
2025-09-26 11:56:05 -0300

    Atualização main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'models/auth_manager.dart';
 import 'screens/pagina_inicial.dart';
 import 'screens/descobrir.dart';
 import 'screens/login.dart';
 import 'screens/cadastro_usuario.dart';
-import 'screens/cadastrar_pedido.dart';
+import 'screens/cadastrar_campanha.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(create: (_) => AuthManager(), child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,17 +20,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Social Action Platform',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: const Color(0xFFf6f6f6),
-      ),
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0xFFf6f6f6)),
       initialRoute: '/',
       routes: {
         '/': (context) => const PaginaInicial(),
         '/descobrir': (context) => const DescobrirPage(),
         '/login': (context) => const LoginPage(),
         '/cadastro-usuario': (context) => const CadastroUsuarioPage(),
-        '/cadastrar-pedido': (context) => const CadastrarPedidoPage(),
+        '/cadastrar-campanha': (context) => const CadastrarCampanhaPage(),
       },
     );
   }
